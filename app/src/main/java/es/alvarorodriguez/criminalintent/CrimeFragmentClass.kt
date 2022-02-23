3c12ac3e23174b64dc868ea3e9d69371f806f7d3
arodriguezs2020 <dam16.2020@gmail.com>
2022-02-23 09:30:21 +0100

    Initial commit

---
+++ app/src/main/java/es/alvarorodriguez/criminalintent/CrimeFragmentClass.kt
@@ -0,0 +1,61 @@
+package es.alvarorodriguez.criminalintent
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class CrimeFragmentClass : Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    private fun loadView(view: View) {
+        titleField = view.findViewById(R.id.crime_title) as EditText
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        loadView(view)
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                // This space intentionally left blank
+            }
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                crime.title = sequence.toString()
+            }
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+        titleField.addTextChangedListener(titleWatcher)
+    }
+}
