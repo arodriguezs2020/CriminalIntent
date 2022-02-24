f55fa05bfe911ee95f1d005324aa970dfa3a1787
arodriguezs2020 <dam16.2020@gmail.com>
2022-02-24 10:04:48 +0100

    Implementando ViewModel

--- app/src/main/java/es/alvarorodriguez/criminalintent/CrimeFragment.kt
+++ app/src/main/java/es/alvarorodriguez/criminalintent/CrimeFragment.kt
@@ -6,13 +6,17 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
-class CrimeFragmentClass : Fragment() {
+class CrimeFragment : Fragment(R.layout.fragment_crime) {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +25,19 @@ class CrimeFragmentClass : Fragment() {
 
     private fun loadView(view: View) {
         titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         loadView(view)
-        return view
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
     }
 
     override fun onStart() {
@@ -42,7 +49,13 @@ class CrimeFragmentClass : Fragment() {
                 count: Int,
                 after: Int
             ) {
-                // This space intentionally left blank
+
+                solvedCheckBox.apply {
+                    setOnCheckedChangeListener { _, isChecked ->
+                        crime.isSolved = isChecked
+                    }
+                }
+
             }
             override fun onTextChanged(
                 sequence: CharSequence?,
